04b73f0c7b910b09b04c408c5a84be4389aad29c
Romina <rowasc@gmail.com>
2020-07-04 06:21:34 -0300

    Fix: get survey, list surveys, send survey post

--- README.md
+++ README.md
@@ -12,3 +12,36 @@
     ]
 ```
 - Install using `composer require ushahidi/platform-sdk-php:dev-master`
+
+
+## Usage
+
+#### Get list of available surveys 
+
+```
+$client = new \PlatformSDK\Ushahidi('http://192.168.33.110', '5');
+$result = $client->getAvailableSurveys()
+
+// $result will be an associative array with the response
+```
+
+
+#### Get a specific survey, fully hydrated with all required entities
+```
+$client = new \PlatformSDK\Ushahidi('http://192.168.33.110', '5');
+// $id is the survey ID which you can get from the list of available surveys
+ 
+$result = $client->getSurvey($id);
+
+// $result will be an associative array with the response
+
+```
+#### Create a new Post
+
+```
+$client = new \PlatformSDK\Ushahidi('http://192.168.33.110', '5');
+$result = $client->createPost($array)
+
+// $result will be an associative array with the response
+```
+
--- src/Client.php
+++ src/Client.php
@@ -3,7 +3,7 @@
 
 namespace PlatformSDK;
 use GuzzleHttp\Client;
-
+use Psr\Http\Message\ResponseInterface;
 class Ushahidi
 {
     private $survey;
@@ -11,14 +11,14 @@ class Ushahidi
     private $apiUrl;
     private $apiVersion = "5";
     private $client;
+    private $resourcesUrl;
     public function __construct(string $apiUrl, $version = "5")
     {
         $this->apiUrl = $apiUrl;
         $this->apiVersion = $version;
-
         $this->client = new Client([
             // Base URI is used with relative requests
-            'base_uri' => "$this->apiUrl/api/v$this->apiVersion",
+            'base_uri' => "$this->apiUrl/api/v$this->apiVersion/",
             // You can set any number of default request options.
             'timeout'  => 2.0,
         ]);
@@ -26,19 +26,18 @@ class Ushahidi
 
     /**
      * Lists all available surveys from the Ushahidi Platform API
-     * @return array
      */
     public function getAvailableSurveys()
     {
-        $url = "/surveys?format=minimal";
-        return $this->client->request(
+        $url = "surveys?format=minimal";
+        return $this->handleResponse($this->client->request(
             'GET',
             $url,
             [
                 'Content-Type' => 'application/json',
                 'Accept'       => 'application/json'
             ]
-        );
+        ));
     }
 
     /**
@@ -47,15 +46,15 @@ class Ushahidi
      */
     public function getSurvey(int $id)
     {
-        $url = "/surveys";
-        return $this->client->request(
+        $url = "surveys";
+        return $this->handleResponse($this->client->request(
             'GET',
             "$url/$id",
             [
                 'Content-Type' => 'application/json',
                 'Accept'       => 'application/json'
             ]
-        );
+        ));
     }
 
     /**
@@ -64,14 +63,30 @@ class Ushahidi
      */
     public function createPost(array $data)
     {
-        $url = "/posts";
-        return $this->client->request(
+        $url = "posts";
+        return $this->handleResponse($this->client->request(
             'POST',
             "$url",
             [
                 'Content-Type' => 'application/json',
-                'Accept'       => 'application/json'
+                'Accept'       => 'application/json',
+                'json' => $data
             ]
-        );
+        ));
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @return array
+     */
+    private function handleResponse(ResponseInterface $response) {
+        $response_code = $response->getStatusCode();
+        $response_reason = $response->getReasonPhrase();
+        $body = $response->getBody()->getContents();
+        return [
+            'body' => json_decode($body, true),
+            'status' => $response_code,
+            'reason' => $response_reason
+        ];
     }
 }
